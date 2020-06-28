ae0510aa478edf0841e6d6a47a3dc5f3436c0b83
jennifer <gwyjennifer@gmail.com>
2020-06-28 13:55:26 +0800

    Update IntList.java

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,10 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null)
+            return B;
+        else if (B == null)
+            return A;
         IntList ptr = A;
         while (ptr.rest != null) {
             ptr = ptr.rest;
@@ -95,6 +99,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null)
+            return B;
+        else if (B == null)
+            return A;
         IntList res = new IntList(A.first, null);
         IntList ptr = res;
         A = A.rest;
